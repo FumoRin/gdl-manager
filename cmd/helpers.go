5468f6ba391e7a7006ea0bc2e920f0cd015c804e
FumoRin <edu.dimasfaizsatria@gmail.com>
2026-06-25 16:54:08 +0700

    feat: migrating reporter to mpb

--- cmd/helpers.go
+++ cmd/helpers.go
@@ -4,43 +4,75 @@ import (
 	"fmt"
 	"os"
 	"os/signal"
+	"sync"
 	"syscall"
 
 	"github.com/fumorin/gdl-manager/internal/downloader"
+	"github.com/vbauerster/mpb/v8"
+	"github.com/vbauerster/mpb/v8/decor"
 )
 
 func RunDownloadSession(m *downloader.DownloadManager) error {
-	progressDone := make(chan struct{})
 	sigChan := make(chan os.Signal, 1)
 	signal.Notify(sigChan, os.Interrupt, syscall.SIGTERM)
 
+	p := mpb.New()
+
+	var bars sync.Map
+	var progressMap sync.Map
+
+	progressDone := make(chan struct{})
+
 	// Print Download Progress
 	go func() {
-		nextIndex := 0
-		lineMap := make(map[string]int)
-		for p := range m.ProgressChan() {
-			if _, ok := lineMap[p.Filename]; !ok {
-				lineMap[p.Filename] = nextIndex
-				nextIndex++
-				fmt.Println()
-			}
+		for prog := range m.ProgressChan() {
+			// Store a copy of the progress to avoid race conditions
+			pCopy := prog
+			progressMap.Store(pCopy.Filename, &pCopy)
 
-			dist := nextIndex - lineMap[p.Filename]
-			fmt.Printf("\033[%dA", dist)
-			fmt.Print("\r")
-			fmt.Printf(
-				"\033[K%-30s | %.2f%% | %-7s/%-7s |  %s/s | ETA %s",
-				downloader.Truncate(p.Filename, 30),
-				p.Percentage,
-				downloader.FormatBytes(p.CurrentSize),
-				downloader.FormatBytes(p.TotalSize),
-				downloader.FormatBytes(int64(p.Speed)),
-				downloader.FormatTime(p.ETA),
-			)
-			fmt.Printf("\033[%dB", dist)
+			if val, ok := bars.Load(pCopy.Filename); ok {
+				bar := val.(*mpb.Bar)
+				bar.SetCurrent(pCopy.CurrentSize)
+			} else {
+				filename := pCopy.Filename
+				bar := p.AddBar(pCopy.TotalSize,
+					mpb.PrependDecorators(
+						decor.Name(downloader.Truncate(filename, 20), decor.WC{W: 20, C: decor.DindentRight}),
+						decor.Percentage(decor.WC{W: 6}),
+					),
+					mpb.AppendDecorators(
+						decor.Any(func(st decor.Statistics) string {
+							if v, ok := progressMap.Load(filename); ok {
+								currProg := v.(*downloader.Progress)
+								return fmt.Sprintf(" %s/%s",
+									downloader.FormatBytes(currProg.CurrentSize),
+									downloader.FormatBytes(currProg.TotalSize),
+								)
+							}
+							return ""
+						}, decor.WC{W: 25, C: decor.DindentRight}),
+						decor.Any(func(st decor.Statistics) string {
+							if v, ok := progressMap.Load(filename); ok {
+								currProg := v.(*downloader.Progress)
+								return fmt.Sprintf("  %s/s", downloader.FormatBytes(int64(currProg.Speed)))
+							}
+							return ""
+						}, decor.WC{W: 15, C: decor.DindentRight}),
+						decor.Any(func(st decor.Statistics) string {
+							if v, ok := progressMap.Load(filename); ok {
+								currProg := v.(*downloader.Progress)
+								return fmt.Sprintf("  ETA %s", downloader.FormatTime(currProg.ETA))
+							}
+							return ""
+						}, decor.WC{W: 30, C: decor.DindentRight}),
+					),
+				)
+				bars.Store(filename, bar)
+				bar.SetCurrent(pCopy.CurrentSize)
+			}
 		}
 
-		fmt.Println()
+		p.Wait()
 		close(progressDone)
 	}()
 
@@ -48,6 +80,7 @@ func RunDownloadSession(m *downloader.DownloadManager) error {
 		sig := <-sigChan
 		fmt.Printf("\nReceived signal %v. Shutting down gracefully...\n", sig)
 		m.Shutdown()
+		p.Shutdown()
 	}()
 
 	m.Wait()
